fb3972d838ed212cf87cc6c03a9a28f763b03afc
matthieu rougeolle <matthieu.rougeolle@gmail.com>
2016-11-09 14:42:53 +0100

    modification de app

--- app.js
+++ app.js
@@ -1,9 +1,36 @@
 (function(){
 
+var app = angular.module('app', []);
 
+app.directive('header', function(){
+	return {
+		restrict : 'A',
+		name : 'header',
+		templateUrl : 'partials/common/header.html'
+	}
+});
 
+app.directive('footer', function(){
+	return {
+		restrict : 'A',
+		name : 'footer',
+		templateUrl : 'partials/common/footer.html'
+	}
+});
 
+app.directive('home', function(){
+	return {
+		restrict : 'A',
+		name : 'home',
+		templateUrl : 'partials/home/home.html'
+	}
+});
 
+app.controller("appController", function(){
+
+	this.personne = object;
+
+});
 
   var object = {
     name : "Tian Bao",
@@ -11,4 +38,5 @@
     skills : ['Manger', 'Jouer'],
     image : 'img/panda.jpg'
   };
+
 })();
